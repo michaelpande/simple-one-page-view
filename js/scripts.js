e8bbc266bf46eedf5a28c915de9d706e89b858ad
Michael Pande <michaelpande@gmail.com>
2017-06-23 16:04:50 +0200

    Okaofkawkfk

--- js/scripts.js
+++ js/scripts.js
@@ -3,30 +3,30 @@
 				overrideEmbed = "https://media.giphy.com/media/xUA7bg8KiG0DKeGAg0/giphy.mp4";
 
 		var embeds = [
-      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 00:00
-      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 01:00
-      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 02:00
-      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 03:00
-      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 04:00
-      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 05:00
-      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 06:00
-      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 07:00
-      "https://www.youtube.com/embed/PPmnFbpnvMQ?autoplay=1", // 08:00
-      "https://www.youtube.com/embed/PPmnFbpnvMQ?autoplay=1", // 09:00
-      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 10:00
-      "https://www.youtube.com/embed/_eBGce6a-0A?autoplay=1", // 11:00
-      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 12:00
-      "https://www.youtube.com/embed/PPmnFbpnvMQ?autoplay=1", // 13:00
-      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 14:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=medium", // 00:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=medium", // 01:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=medium", // 02:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=medium", // 03:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=medium", // 04:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=medium", // 05:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=medium", // 06:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=medium", // 07:00
+      "https://www.youtube.com/embed/PPmnFbpnvMQ?autoplay=1&vq=medium", // 08:00
+      "https://www.youtube.com/embed/PPmnFbpnvMQ?autoplay=1&vq=medium", // 09:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=medium", // 10:00
+      "https://www.youtube.com/embed/_eBGce6a-0A?autoplay=1&vq=medium", // 11:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=medium", // 12:00
+      "https://www.youtube.com/embed/PPmnFbpnvMQ?autoplay=1&vq=medium", // 13:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=medium", // 14:00
       "https://www.youtube.com/embed/PPmnFbpnvMQ?autoplay=1&vq=medium", // 15:00
       "https://www.youtube.com/embed/PPmnFbpnvMQ?autoplay=1&vq=medium", // 16:00
       "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=medium", // 17:00
-      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 18:00
-      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 19:00
-      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 20:00
-      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 21:00
-      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 22:00
-      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 23:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=medium", // 18:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=medium", // 19:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=medium", // 20:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=medium", // 21:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=medium", // 22:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=medium", // 23:00
 		]
 
     function setContent(){
@@ -47,8 +47,8 @@
 		setInterval(setContent, 10000);
 
 
-    // Reload every 30 min
+    // Reload every hour
     setInterval(function(){
       location.reload();
-    },1300000);
+    },2600000);
 })();
