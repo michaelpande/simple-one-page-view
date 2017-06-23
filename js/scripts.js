b161d54d3149d2a64129f05dec3076d131f5ca6e
Michael Pande <michaelpande@gmail.com>
2017-06-23 14:32:54 +0200

    koawfoawk

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,63 @@
 (function(){
-		var override = true,
-				overrideVideo = "https://media.giphy.com/media/xUA7bg8KiG0DKeGAg0/giphy.mp4",
-        overrideText = "";
+		var override = false,
+				overrideEmbed = "https://media.giphy.com/media/xUA7bg8KiG0DKeGAg0/giphy.mp4";
 
-		var animations = [
-			{video: "https://media.giphy.com/media/xUA7b4arnbo3THfzi0/giphy.mp4", text: ""}, // 0:00
- 			{video: "https://media.giphy.com/media/xUA7b4arnbo3THfzi0/giphy.mp4", text: ""}, // 1:00
-			{video: "https://media.giphy.com/media/xUA7b4arnbo3THfzi0/giphy.mp4", text: ""}, // 2:00
- 			{video: "https://media.giphy.com/media/xUA7b4arnbo3THfzi0/giphy.mp4", text: ""}, // 3:00
-			{video: "https://media.giphy.com/media/xUA7b4arnbo3THfzi0/giphy.mp4", text: ""}, // 4:00
- 			{video: "https://media.giphy.com/media/xUA7b4arnbo3THfzi0/giphy.mp4", text: ""}, // 5:00
-			{video: "https://media.giphy.com/media/xUA7b4arnbo3THfzi0/giphy.mp4", text: ""}, // 6:00
- 			{video: "https://media.giphy.com/media/xUA7b4arnbo3THfzi0/giphy.mp4", text: ""}, // 7:00
-			{video: "https://media.giphy.com/media/xUA7b4arnbo3THfzi0/giphy.mp4", text: ""}, // 8:00
- 			{video: "https://media.giphy.com/media/xUA7b4arnbo3THfzi0/giphy.mp4", text: ""}, // 9:00
-			{video: "https://media.giphy.com/media/xUA7b4arnbo3THfzi0/giphy.mp4", text: ""}, // 10:00
- 			{video: "https://media.giphy.com/media/xUA7b4arnbo3THfzi0/giphy.mp4", text: ""}, // 11:00
-			{video: "https://media.giphy.com/media/xUA7b4arnbo3THfzi0/giphy.mp4", text: ""}, // 12:00
- 			{video: "https://media.giphy.com/media/xUA7b4arnbo3THfzi0/giphy.mp4", text: ""}, // 13:00
-			{video: "https://media.giphy.com/media/xUA7b4arnbo3THfzi0/giphy.mp4", text: ""}, // 14:00
- 			{video: "https://media.giphy.com/media/xUA7b4arnbo3THfzi0/giphy.mp4", text: ""}, // 15:00
-			{video: "https://media.giphy.com/media/xUA7b4arnbo3THfzi0/giphy.mp4", text: ""}, // 16:00
- 			{video: "https://media.giphy.com/media/xUA7b4arnbo3THfzi0/giphy.mp4", text: ""}, // 17:00
-			{video: "https://media.giphy.com/media/xUA7b4arnbo3THfzi0/giphy.mp4", text: ""}, // 18:00
- 			{video: "https://media.giphy.com/media/xUA7b4arnbo3THfzi0/giphy.mp4", text: ""}, // 19:00
-			{video: "https://media.giphy.com/media/xUA7b4arnbo3THfzi0/giphy.mp4", text: ""}, // 20:00
- 			{video: "https://media.giphy.com/media/xUA7b4arnbo3THfzi0/giphy.mp4", text: ""}, // 21:00
-			{video: "https://media.giphy.com/media/xUA7b4arnbo3THfzi0/giphy.mp4", text: ""}, // 22:00
- 			{video: "https://media.giphy.com/media/xUA7b4arnbo3THfzi0/giphy.mp4", text: ""} // 23:00
+		var embeds = [
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 23:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 23:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 23:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 23:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 23:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 23:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 23:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 23:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 23:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 23:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 23:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 23:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 23:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 23:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 23:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 23:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 23:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 23:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 23:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 23:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 23:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 23:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 23:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 23:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 23:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 23:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 23:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 23:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 23:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 23:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 23:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 23:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 23:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 23:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 23:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 23:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 23:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1", // 23:00
 		]
 
-    function setAnimation(){
+    function setContent(){
       var hour = new Date().getHours();
-			videoContainer = document.getElementById('videoContainer');
-      videoText = document.getElementById('videoText');
+			content = document.getElementById('content');
 
 			if(override){
-        if(videoContainer.src !== overrideVideo){
-      	  videoContainer.src = overrideVideo;
-          videoText.innerHTML = overrideText;
+        if(content.src !== overrideEmbed){
+      	  content.src = overrideEmbed;
         }
 				return;
 			}
-			if(videoContainer.src !== animations[hour].video){
-      	videoContainer.src = animations[hour].video;
-        videoText.innerHTML = animations[hour].text;
+			if(content.src !== embeds[hour]){
+      	content.src = embeds[hour];
 			}
 		}
 
-		setInterval(setAnimation, 1000);
+		setInterval(setContent, 1000);
 
 })();
