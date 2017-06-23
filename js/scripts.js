a44ff7fa1e98e3087ca7b0ef80b6d019da9c05f6
Michael Pande <michaelpande@gmail.com>
2017-06-23 16:30:05 +0200

    kaowfkoawfowak

--- js/scripts.js
+++ js/scripts.js
@@ -3,30 +3,30 @@
 				overrideEmbed = "https://media.giphy.com/media/xUA7bg8KiG0DKeGAg0/giphy.mp4";
 
 		var embeds = [
-      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=small", // 00:00
-      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=small", // 01:00
-      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=small", // 02:00
-      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=small", // 03:00
-      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=small", // 04:00
-      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=small", // 05:00
-      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=small", // 06:00
-      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=small", // 07:00
-      "https://www.youtube.com/embed/PPmnFbpnvMQ?autoplay=1&vq=small", // 08:00
-      "https://www.youtube.com/embed/PPmnFbpnvMQ?autoplay=1&vq=small", // 09:00
-      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=small", // 10:00
-      "https://www.youtube.com/embed/_eBGce6a-0A?autoplay=1&vq=small", // 11:00
-      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=small", // 12:00
-      "https://www.youtube.com/embed/PPmnFbpnvMQ?autoplay=1&vq=small", // 13:00
-      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=small", // 14:00
-      "https://www.youtube.com/embed/PPmnFbpnvMQ?autoplay=1&vq=small", // 15:00
-      "https://www.youtube.com/embed/PPmnFbpnvMQ?autoplay=1&vq=small", // 16:00
-      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=small", // 17:00
-      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=small", // 18:00
-      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=small", // 19:00
-      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=small", // 20:00
-      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=small", // 21:00
-      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=small", // 22:00
-      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=small", // 23:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=default", // 00:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=default", // 01:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=default", // 02:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=default", // 03:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=default", // 04:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=default", // 05:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=default", // 06:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=default", // 07:00
+      "https://www.youtube.com/embed/PPmnFbpnvMQ?autoplay=1&vq=default", // 08:00
+      "https://www.youtube.com/embed/PPmnFbpnvMQ?autoplay=1&vq=default", // 09:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=default", // 10:00
+      "https://www.youtube.com/embed/_eBGce6a-0A?autoplay=1&vq=default", // 11:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=default", // 12:00
+      "https://www.youtube.com/embed/PPmnFbpnvMQ?autoplay=1&vq=default", // 13:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=default", // 14:00
+      "https://www.youtube.com/embed/PPmnFbpnvMQ?autoplay=1&vq=default", // 15:00
+      "https://www.youtube.com/embed/PPmnFbpnvMQ?autoplay=1&vq=default", // 16:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=default", // 17:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=default", // 18:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=default", // 19:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=default", // 20:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=default", // 21:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=default", // 22:00
+      "https://www.youtube.com/embed/UjALrXRNzGE?autoplay=1&vq=default", // 23:00
 		]
 
     function setContent(){
